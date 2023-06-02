11135738d26e24c53e7133209c2b3cf62d2ea415
rohanchau1111 <chaudharirohan938@gmail.com>
2023-06-02 15:33:47 +0530

    repo changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom/client';
-import { reportWebVitals } from 'reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { store } from 'store';
-import { Routes } from 'routes';
-import { SnackbarWrapper } from 'components/shared/snackbar-wrapper';
-
-import 'animate.css';
-import 'styles/index.scss';
-
-const rootElement = document.querySelector('#root');
-
-if (!rootElement) throw new Error('Failed to find the root element');
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <SnackbarWrapper />
-      <Routes />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
